1bfa20221e439bb9f8d2af1ce7401632584141a4
Pradeep CE <breathingcode@gmail.com>
2017-02-09 00:34:17 +0530

    Fix pep8 errors in default config file
    This actually decreases the readability of the code. There is no
    sane reason this change should've been made except that this is
    a python file, and it could be pep8-compliant.

--- app/config.default.py
+++ app/config.default.py
@@ -16,18 +16,26 @@ TWITTER_MEDIA_UPLOAD_URL = 'https://upload.twitter.com/1.1/media/upload.json'
 
 # URLs for external styles and JS.
 JQUERY_URL = 'https://code.jquery.com/jquery-3.1.1.slim.min.js'
-JQUERY_SUM = 'sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n'
+JQUERY_SUM = 'sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3h' + \
+    'pfag6Ed950n'
 
-BOOTSTRAP_CSS_URL = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css'
-BOOTSTRAP_CSS_SUM = 'sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ'
+BOOTSTRAP_CSS_URL = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha' + \
+    '.6/css/bootstrap.min.css'
+BOOTSTRAP_CSS_SUM = 'sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQ' + \
+    'QKnKkoFVhFQhNUwEyJ'
 
-BOOTSTRAP_JS_URL = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js'
-BOOTSTRAP_JS_SUM = 'sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn'
+BOOTSTRAP_JS_URL = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.' + \
+    '6/js/bootstrap.min.js'
+BOOTSTRAP_JS_SUM = 'sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVk' + \
+    'E+jo0ieGizqPLForn'
 
-TETHER_JS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js'
-TETHER_JS_SUM = 'sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb'
+TETHER_JS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/' + \
+    'tether.min.js'
+TETHER_JS_SUM = 'sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx' + \
+    '6Qin1DkWx51bBrb'
 
-HTML2CANVAS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js'
+HTML2CANVAS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/' + \
+    '0.4.1/html2canvas.min.js'
 HTML2CANVAS_SUM = 'sha256-c3RzsUWg+y2XljunEQS0LqWdQ04X1D3j22fd/8JCAKw='
 
 FONT_URL = 'https://fonts.googleapis.com/css?family=Source+Sans+Pro'
